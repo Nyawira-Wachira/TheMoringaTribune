309e4be0b6f58f976cf9251fb1283e4b130bbf01
ABBY <abbyW@moringa.com>
2022-05-27 17:01:20 +0300

    Updates.

--- tribune/settings.py
+++ tribune/settings.py
@@ -100,11 +100,12 @@ else:
        )
    }
 
-   db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
- # 'NAME': 'tribune',
+
+#  # 'NAME': 'tribune',
         # 'USER': 'folio',
         # 'PASSWORD':'123abby',
 # Password validation
